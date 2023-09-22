48927037cb685e7ab73685526b05b49845f71f87
rangga <ranggashadow27@gmail.com>
2023-09-22 19:12:04 +0700

    pages and route is ready

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages/blank-page', ['type_menu' => '']);
+Route::get('/', function() {
+    return view('pages.app.dashboard-siakad' , ['type_menu' => '']);
 });
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register');
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('pages.auth.auth-forgot-password');
+})->name('forgot-password');
+
+Route::get('/reset-password', function() {
+    return view('pages.auth.auth-reset-password');
+})->name('reset-password');
